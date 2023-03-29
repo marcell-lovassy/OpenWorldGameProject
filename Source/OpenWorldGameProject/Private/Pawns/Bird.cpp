323914bd99cfbd71a9f62eca18852036c68df4ec
marcell-lovassy <marcell.lovassy@gmail.com>
2023-03-29 16:02:26 +0200

    Create EnhancedInputSystem for basic flying movement

--- Source/OpenWorldGameProject/Private/Pawns/Bird.cpp
+++ Source/OpenWorldGameProject/Private/Pawns/Bird.cpp
@@ -3,6 +3,13 @@
 
 #include "Pawns/Bird.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ABird::ABird()
@@ -13,14 +20,40 @@ ABird::ABird()
 	Capsule->SetCapsuleHalfHeight(20.f);
 	Capsule->SetCapsuleRadius(15.f);
 	SetRootComponent(Capsule);
+
+	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(FName("BirdMesh"));
+	BirdMesh->SetupAttachment(GetRootComponent());
+
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(FName("SpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+
+	SpringArm->TargetArmLength = 200.f;
+	SpringArm->SetWorldRotation(FRotator(-25.f, 0.f, 0.f));
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 20.f;
+
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(FName("ViewCamera"));
+	ViewCamera->SetupAttachment(SpringArm);
+
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
 void ABird::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (APlayerController* playerController = Cast<APlayerController>(GetController())) 
+	{
+		if(UEnhancedInputLocalPlayerSubsystem* subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(playerController->GetLocalPlayer()))
+		{
+			subsystem->AddMappingContext(BirdMappingContext, 0);
+		}
+	}
 }
+
 
 // Called every frame
 void ABird::Tick(float DeltaTime)
@@ -33,6 +66,34 @@ void ABird::Tick(float DeltaTime)
 void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	if(UEnhancedInputComponent* enhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		enhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABird::Move);
+		enhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered, this, &ABird::Turn);
+	}
+
+	//Old way
+	//PlayerInputComponent->BindAxis(FName("MoveForward"), this, &ABird::MoveForward);
 
 }
 
+void ABird::Move(const FInputActionValue& value)
+{
+	float movementValue = value.Get<float>();
+	FVector forward = GetActorForwardVector();
+	AddMovementInput(forward, movementValue);
+}
+
+void ABird::Turn(const FInputActionValue& value)
+{
+	FVector2D turnValue = value.Get<FVector2D>();
+	AddControllerYawInput(turnValue.X * MouseSensitivity);
+	//AddControllerRollInput(turnValue.X / 2.f);
+	AddControllerPitchInput((InvertY ? -turnValue.Y: turnValue.Y) * MouseSensitivity);
+}
+
+void ABird::MoveForward(float value)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Value %f"), value);
+}
--- Source/OpenWorldGameProject/Public/Pawns/Bird.h
+++ Source/OpenWorldGameProject/Public/Pawns/Bird.h
@@ -4,7 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "Bird.generated.h"
+
+class UCapsuleComponent;
+class USkeletalMeshComponent;
+class UInputMappingContext;
+class UInputAction;
+class USpringArmComponent;
+class UCameraComponent;
 
 UCLASS()
 class OPENWORLDGAMEPROJECT_API ABird : public APawn
@@ -19,8 +27,32 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	float MouseSensitivity = 1.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	bool InvertY = false;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* BirdMappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* TurnAction;
+
+	//Enhanced input system (new)
+	void Move(const FInputActionValue& value);
+	void Turn(const FInputActionValue& value);
+
+	//Old input system
+	void MoveForward(float value);
+
 private:
 	UPROPERTY(VisibleAnywhere)
-	class UCapsuleComponent* Capsule;
-
+	UCapsuleComponent* Capsule;
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* BirdMesh;
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* ViewCamera;
 };
